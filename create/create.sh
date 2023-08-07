552fff780e3c68c1905f0df11b633793e99714e2
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-08-07 20:58:53 +0530

    Update create.sh

--- create/create.sh
+++ create/create.sh
@@ -22,8 +22,11 @@ sudo apt update -y
 
 sudo apt install -y docker.io
 
-sudo apt install -y docker-compose
+sudo apt install curl -y
 
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
 
 EOF
 
